e09ec341db24ba99a945f82b443cac04a202fb13
iZrg <mickaelfrost@gmail.com>
2020-09-12 13:24:37 +0300

    Added persistent scene loader

---
+++ foodbattle/Assets/Scripts/Editor/PersistentSceneLoader.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[InitializeOnLoad]
+public class PersistentSceneLoader
+{
+    static PersistentSceneLoader()
+    {
+        EditorApplication.playModeStateChanged += EditorApplicationOnplayModeStateChanged;
+    }
+
+    private static void EditorApplicationOnplayModeStateChanged(PlayModeStateChange state)
+    {
+        Debug.Log(state);
+    }
+}
